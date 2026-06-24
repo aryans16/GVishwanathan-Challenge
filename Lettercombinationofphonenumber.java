2f435fb36fcd628e9a2745a523c6426fd6fa384d
Aryan Sharma <aryan161104@gmail.com>
2026-06-24 18:48:22 +0530

    Leetcode problem

---
+++ Lettercombinationofphonenumber.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lettercombinationofphonenumber {
+     public static void main(String[] args) {
+
+        Solution sol = new Solution();
+
+        String digits = "23";
+
+        List<String> ans = sol.letterCombinations(digits);
+
+        System.out.println(ans);
+    }
+    
+}
+
+class Solution{
+     public List<String> letterCombinations(String digits){
+        List<String> result = new ArrayList<>();
+
+        if(digits == null || digits.length() ==0){
+            return result;
+        }
+
+       String[] map = {
+          "", "",
+          "abc", "def", "ghi",
+          "jkl", "mno", "pqrs",
+          "tuv", "wxyz"
+        };
+
+        backtrack(digits,0,new StringBuilder(), result , map);
+        return result;
+     }
+
+     private void backtrack(String digits, int index, StringBuilder current, List<String> result, String[] map){
+        if( index == digits.length()){
+            result.add(current.toString());
+            return;
+        }
+
+        String letters = map[digits.charAt(index) - '0'];
+
+        for(char ch : letters.toCharArray()){
+            current.append(ch);
+
+            backtrack(digits, index + 1, current, result, map);
+
+            current.deleteCharAt(current.length() - 1);
+
+        }
+     }
+}
