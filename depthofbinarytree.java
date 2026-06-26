bc731b8500a8028e899ae9d985aa6ad6f272a83e
Aryan Sharma <aryan161104@gmail.com>
2026-06-26 15:48:24 +0530

    Leetcode problem

---
+++ depthofbinarytree.java
@@ -0,0 +1,44 @@
+public class depthofbinarytree {
+    public static void main(String[] args){
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        System.out.println("Maximum Depth: " + depth_solution.maxDepth(root));
+
+    }
+    
+}
+
+class depth_solution{
+     public static int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ removeNthnode.java
@@ -0,0 +1,63 @@
+public class removeNthnode {
+    public static void main(String[] args) {
+
+        remove_solution sol = new remove_solution();
+
+        // 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        int n = 2;
+
+        System.out.print("Original List: ");
+        sol.printlist(head);
+
+        head = sol.removeNthFromEnd(head, n);
+
+        System.out.print("After Removal: ");
+        sol.printlist(head);
+    }
+    
+}
+
+class remove_solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+
+    public void printlist(ListNode head){
+        while(head != null){
+            System.out.println(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+}
+
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(){}
+    ListNode(int val){this.val = val;}
+    ListNode(int val, ListNode next){ this.val = val ; this.next = next;}
+}
