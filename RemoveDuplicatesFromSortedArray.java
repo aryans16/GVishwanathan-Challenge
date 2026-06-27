ddcbe1f3c3fb14dfa2d01f3cddc1712ab2d11bb6
Aryan Sharma <aryan161104@gmail.com>
2026-06-27 14:24:56 +0530

    Leetcode problem

---
+++ RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,38 @@
+
+
+public class RemoveDuplicatesFromSortedArray {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+
+        int k = sol.removeDuplicates(nums);
+
+        System.out.println("Number of unique elements: " + k);
+
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int i = 0;
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+    }
+}
